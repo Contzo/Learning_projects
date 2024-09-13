4c9b1a8b53beda9a83cf57c105d3e20606e2c8d2
Contzo <ilierazvan1497@outlook.com>
2024-09-13 12:08:03 +0300

    Added Countries List

--- WorldWise/src/components/CountriesList.jsx
+++ WorldWise/src/components/CountriesList.jsx
@@ -1,16 +1,23 @@
 import styles from "./CountriesList.module.css";
 import Spinner from "./Spinner";
-import CityItem from "./CityItem";
+import CountryItem from "./CountryItem";
 import Message from "./Message";
 /* eslint-disable react/prop-types */
 function CountriesList({ cities, isLoading }) {
+  // return the spinner if the data is still loading
   if (isLoading) return <Spinner />;
+  // If there are no visited city display a message
   if (!cities.length) return <Message message="Add your first city" />;
-  const countries = [];
+  // extract the unique countries from the cities prop
+  const countries = cities.reduce((array, city) => {
+    if (!array.map((el) => el.country).includes(city.country)) {
+      return [...array, { country: city.country, emoji: city.emoji }];
+    } else return array;
+  }, []);
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem city={country} />
+        <CountryItem country={country} key={cities.id} />
       ))}
     </ul>
   );

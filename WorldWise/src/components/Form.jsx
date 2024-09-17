74f78e0155d3a4d2adf37eb5dd89eaf8aca5d11c
Contzo <ilierazvan1497@outlook.com>
2024-09-17 15:50:00 +0300

    Added a map, geolocation, interaction with the map and reverse geocodding

--- WorldWise/src/components/Form.jsx
+++ WorldWise/src/components/Form.jsx
@@ -1,10 +1,14 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 
 import styles from "./Form.module.css";
 import { useNavigate } from "react-router-dom";
+import { useURLPosition } from "../hooks/useURLPosition";
+import { map } from "leaflet";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -14,14 +18,46 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client?";
 function Form() {
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
-
+  const [lat, lng] = useURLPosition();
   const navigate = useNavigate();
+  const [isLoadingGeocode, setIsLoadingGeocode] = useState(false);
+  const [geocodingError, setGeocodingError] = useState("");
 
+  const [emoji, setEmoji] = useState("");
+  useEffect(
+    function () {
+      async function fetchCityData(params) {
+        try {
+          setIsLoadingGeocode(true);
+          setGeocodingError(""); // reset error
+          const response = await fetch(
+            `${BASE_URL}latitude=${lat}&longitude=${lng}`
+          );
+          const data = await response.json();
+          if (!data.countryCode)
+            throw new Error("Click on a valid city, dumb ass!");
+          setCityName(data.city || data.locality || "");
+          setCountry(data.countryName);
+          setEmoji(convertToEmoji(data.countryCode));
+        } catch (error) {
+          setGeocodingError(error.message);
+        } finally {
+          setIsLoadingGeocode(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng]
+  );
+
+  if (isLoadingGeocode) return <Spinner />;
+  if (geocodingError) return <Message message={geocodingError} />;
   return (
     <form className={styles.form}>
       <div className={styles.row}>
@@ -31,7 +67,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
--- WorldWise/src/components/Map.jsx
+++ WorldWise/src/components/Map.jsx
@@ -1,31 +1,94 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CiteisContext";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import { useURLPosition } from "../hooks/useURLPosition";
 function Map() {
-  const navigate = useNavigate(); // Navigation
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { cities } = useCities(); // read the cities from the context
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const {
+    isLoading: isLoadingGeolocation,
+    position: geolocation,
+    getPosition,
+  } = useGeolocation(); // get the current location
+  const [mapLat, mapLng] = useURLPosition();
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  // sync geolocation with the position centered on the Map.
+  useEffect(
+    function () {
+      if (geolocation) setMapPosition([geolocation.lat, geolocation.lng]);
+    },
+    [geolocation]
+  );
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button
-        onClick={(e) => {
-          setSearchParams({ lat: 24, lng: 50 });
-          e.stopPropagation();
-        }}
+    <div className={styles.mapContainer}>
+      {/* custom button component to get the current location  */}
+      {!geolocation && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingGeolocation ? "Loading..." : "Use your position "}
+        </Button>
+      )}
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
 }
 
+/* eslint-disable react/prop-types */
+function ChangeCenter({ position }) {
+  const map = useMap(); // use Map hook to get the instance of the current displayed map
+  map.setView(position, 6); // change the center of the map to the given position
+  return null; // return null since we don't want this component to display something on the UI
+}
+
+function DetectClick() {
+  const navigate = useNavigate(); // Navigation
+  useMapEvents({
+    click: (e) => {
+      // get the coordinates of the point from the event  leaflet event
+      const pointLat = e.latlng.lat;
+      const pointLng = e.latlng.lng;
+      navigate(`form?lat=${pointLat}&lng=${pointLng}`);
+    },
+  });
+}
 export default Map;

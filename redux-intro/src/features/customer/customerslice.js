b39adfc3675ea4abd4d18a827a468984b05f6ed4
Contzo <ilierazvan1497@outlook.com>
2024-09-27 12:14:35 +0300

    Added profesional file structure

---
+++ redux-intro/src/features/customer/customerslice.js
@@ -0,0 +1,41 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+// costumer action creators
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(), // we cant do this in the pure reducer function
+    },
+  };
+}
+function updateName(fullName) {
+  return {
+    type: "costumer/updateName",
+    payload: fullName,
+  };
+}
+
+export { createCustomer, updateName };
--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import { deposit } from "./features/account/accountSlice";
+import { createCustomer } from "./features/customer/customerslice";
+import store from "./store";
+
+store.dispatch(createCustomer("Muila", "12345"));
+store.dispatch(deposit(100));
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

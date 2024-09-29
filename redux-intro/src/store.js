64f6cdc70b80c9361d006ef1f1be9720d102b425
Contzo <ilierazvan1497@outlook.com>
2024-09-29 17:43:24 +0300

    Created a store using RTK

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,15 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerslice";
 
-// root reducer
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-// creating the store
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;

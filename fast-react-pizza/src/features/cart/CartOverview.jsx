52337568f5b40d6ab7ba04aabdf40b882f0c617e
Contzo <ilierazvan1497@outlook.com>
2024-10-06 15:23:17 +0300

    Responsive design

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function CartOverview() {
   return (
-    <div className="text-stone-bg-pink-200 bg-stone-800 p-4 uppercase">
+    <div className="text-stone-bg-pink-200 sm:pxy-6 bg-stone-800 px-4 py-4 text-sm uppercase sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>

a5ac2bde4c28fffc5985ce69eaf6befd431e0267
Contzo <ilierazvan1497@outlook.com>
2024-10-09 15:59:48 +0300

    Building cart overview #1

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 function CartOverview() {
+  const totalCartQuanity = useSelector((state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0),
+  );
   return (
     <div className="text-stone-bg-pink-200 sm:pxy-6 flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300">

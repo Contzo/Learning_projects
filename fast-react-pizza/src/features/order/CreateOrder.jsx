3058e2b9b1629d90b7c6283ec6645ca0265c89ac
Contzo <ilierazvan1497@outlook.com>
2024-10-14 13:27:29 +0300

    Async Thunk

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getCart,
   getTotalCartPizzas,
@@ -12,6 +12,7 @@ import store from "../../store";
 import { clearCart } from "../cart/cartSlice";
 import { useState } from "react";
 import { formatCurrency } from "../../utilities/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -28,12 +29,13 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
   const userName = useSelector((state) => state.user.userName);
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   if (cart.length === 0) return <EmptyCart />;
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>

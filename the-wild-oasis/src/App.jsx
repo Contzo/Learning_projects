2d85d358b82867fb6180ef78c51e964bf8078d7a
Contzo <ilierazvan1497@outlook.com>
2024-10-16 15:53:27 +0300

    Added global styels and styles tokens

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,38 +1,27 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
 const StyledApp = styled.main`
   background-color: orangered;
   padding: 20px;
 `;
 function App() {
   return (
-    <StyledApp>
-      <H1>The wild Oasis</H1>
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Input type="number" placeholder="Guests number" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Input type="number" placeholder="Guests number" />
+      </StyledApp>
+    </>
   );
 }
 

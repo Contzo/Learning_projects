29f712d580a9f65ce2a2cd0c1cd57877170ae4e6
Contzo <ilierazvan1497@outlook.com>
2024-10-21 15:58:24 +0300

    Finished cabin fetching and delting

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -10,3 +10,8 @@ export async function getCabins() {
   }
   return cabins;
 }
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) throw new Error("Cabin could not be deleted");
+}

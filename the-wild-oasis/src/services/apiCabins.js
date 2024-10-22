3ab7907eca396151c78929909716f38487b8ec1f
Contzo <ilierazvan1497@outlook.com>
2024-10-22 16:25:30 +0300

    Creating a new cabin #1

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -15,3 +15,11 @@ export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) throw new Error("Cabin could not be deleted");
 }
+
+export async function createCabin(newCabinObject) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabinObject])
+    .select();
+  if (error) throw new Error("Cabin could not be created");
+}

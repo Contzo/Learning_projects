4551ffe6b38a1e991c0cad6b12a135db2d10fedd
Contzo <ilierazvan1497@outlook.com>
2024-11-08 15:02:57 +0200

    Client side filtering

--- the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
+++ the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -4,7 +4,14 @@ import Filter from "../../ui/Filter";
 function CabinTableOperations() {
   return (
     <TableOperations>
-      <Filter />
+      <Filter
+        filterFieldParam="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
     </TableOperations>
   );
 }

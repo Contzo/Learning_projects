1ce980dc4f8d9a7dcbfa94368966f3296a6ff0fa
Contzo <ilierazvan1497@outlook.com>
2024-11-11 14:19:12 +0200

    API side filtering of the bookings table

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  // Filter
+  const filterStatus = searchParams.get("status");
+  const filterObject =
+    !filterStatus || filterStatus === "all"
+      ? null
+      : { field: "status", value: filterStatus };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filterObject],
+    queryFn: () => getBookings({ filter: filterObject }),
   });
   return { isLoading, bookings, error };
 }

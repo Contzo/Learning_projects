f936dc4734d9521e0afaf147b7c36f21ec504b1c
Contzo <ilierazvan1497@outlook.com>
2024-11-25 00:16:03 +0200

    Protected Routes

--- the-wild-oasis/src/features/authentication/useLogin.js
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,14 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as apiLogin } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isLoading: isLoggingIn } = useMutation({
     mutationFn: ({ email, password }) => apiLogin({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueriesData(["user"], user); // manually set the cache
       navigate("/dashboard ");
     },
     onError: (error) => {
--- the-wild-oasis/src/services/apiAuth.js
+++ the-wild-oasis/src/services/apiAuth.js
@@ -9,3 +9,15 @@ export async function login({ email, password }) {
   console.log(data);
   return data;
 }
+
+export async function getCurrentUser() {
+  // extract the data about the the current session (JWT and user) from the local storage of the browser.
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+  // get the data from Supabase again, instead of using the one from the local storage, because it might become corrupt and we want to make sure that we have the correct use from DB
+  const { data, error } = await supabase.auth.getUser(); // get the user from the DB again for the current session.
+
+  if (error) throw new Error(error.message);
+  console.log(data);
+  return data?.user;
+}

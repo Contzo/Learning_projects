6ff3ede1b2dead4d93e80b73c186aba4da4b87d2
Contzo <ilierazvan1497@outlook.com>
2024-12-03 13:59:28 +0200

    Finished pie chart

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         numCabins={cabins.length}
       />
       <div>Today Activity</div>
-      <div>Chart stat durations</div>
+      <DurationChart confirmedStays={stays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );

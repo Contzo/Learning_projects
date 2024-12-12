d34dcc7f1762dc005acd18e56d36c4795f4cf4e5
Contzo <ilierazvan1497@outlook.com>
2024-12-12 21:52:09 +0200

    Navigatingn between pages #1

--- the-wild-oasis-website/app/page.js
+++ the-wild-oasis-website/app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Welcome to the wild nature paradise</h1>;
+  return (
+    <div>
+      <h1>Welcome to the wild nature paradise</h1>
+      <Link href="/cabins">Explore the cabins</Link>
+    </div>
+  );
 }

2e72a527d4117e9e681bad61bfa10f8b2ec6b09f
Contzo <ilierazvan1497@outlook.com>
2024-12-17 14:30:06 +0100

    Adding interactivity

--- the-wild-oasis-website/app/cabins/page.js
+++ the-wild-oasis-website/app/cabins/page.js
@@ -1,8 +1,18 @@
-export const metadata = {
-  title: "Test",
-  description: "Learn more about our company.",
-};
+import Counter from "../components/Counter";
 
-export default function Page() {
-  return <h1>Cabins page</h1>;
+export default async function Page() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users").then(
+    (res) => res.json()
+  );
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
 }

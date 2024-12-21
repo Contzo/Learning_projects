436b4252dfb3a7ac2893a26025e644cd81752a7d
Contzo <ilierazvan1497@outlook.com>
2024-12-21 16:14:55 +0100

    Create account nested routes

--- the-wild-oasis-website/app/account/profile/page.js
+++ the-wild-oasis-website/app/account/profile/page.js
@@ -45,7 +45,7 @@ export default function Page() {
 
           <SelectCountry
             name="nationality"
-            id="nationality"
+            id={nationality}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             defaultCountry={nationality}
           />

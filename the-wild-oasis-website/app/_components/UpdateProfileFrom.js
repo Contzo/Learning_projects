d81ba4f99810de9fe1dcede0e2d403d94f856081
Contzo <ilierazvan1497@outlook.com>
2025-01-14 12:37:50 +0200

    Displaying laodign indicator while profile is updating

--- the-wild-oasis-website/app/_components/UpdateProfileFrom.js
+++ the-wild-oasis-website/app/_components/UpdateProfileFrom.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
 import { updateProfile } from "../_lib/actions";
 
 function UpdateProfileFrom({ children, guest }) {
@@ -50,13 +51,22 @@ function UpdateProfileFrom({ children, guest }) {
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-
-      <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
-      </div>
+      <Button />
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <div className="flex justify-end items-center gap-6">
+      <button
+        disabled={pending}
+        className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      >
+        {pending ? "Updating..." : "Update profile"}
+      </button>
+    </div>
   );
 }
 
